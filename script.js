727ae063816eea8b900c937d44dc61e3221789a7
nikson1701 <78842306+nikson1701@users.noreply.github.com>
2021-06-03 11:13:15 +0300

    Догружены незагрузившиеся файлы

---
+++ script.js
@@ -0,0 +1,18 @@
+let navigationMenu = document.querySelector('.navigationmenu');
+let navigationMenuOpen = document.querySelector('.navigationmenu-open');
+navigationMenuOpen.onclick = function () {
+	if (navigationMenuOpen.checked){
+		navigationMenu.classList.add('display');
+	} else {
+		navigationMenu.classList.remove('display');
+	}
+    
+    let navigationMenuClose = navigationMenu.querySelector('.navigationmenu-close');
+    navigationMenuClose.onclick = function () {
+        navigationMenu.classList.remove('display');
+		navigationMenuOpen.checked = false;
+    }
+}
+
+let numberOfItemsInCart = document.querySelector('.menu__list-cart-number');
+numberOfItemsInCart.textContent = 3;
