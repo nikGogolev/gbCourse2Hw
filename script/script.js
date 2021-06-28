e9ae0cbeec07a2ac4ed72da1b64bfaccbb84f768
nikson1701 <78842306+nikson1701@users.noreply.github.com>
2021-06-28 14:40:21 +0300

    Доработка скрипта
    Сделано так, чтобы на мобильной версии иконки Аккаунта и Корзины (пропавшие с макета) появлялись при нажатии на меню

--- script/script.js
+++ script/script.js
@@ -3,15 +3,29 @@ var sheetTitle = document.querySelector('title').textContent;
 var navigationMenu = document.querySelector('.navmenu');
 var navigationMenuOpen = document.querySelector('.navmenu-open');
 var navigationMenuClose = document.querySelector('.navmenu-close');
+
+var menuListItemAccount = document.querySelector('.menu-list-item-account');
+var menuListItemCart = document.querySelector('.menu-list-item-cart');
+
 navigationMenuOpen.onclick = function () {
 	if (navigationMenuOpen.checked){
 		navigationMenu.style.height = '764px';
+		if (window.screen.width <= 375){
+			menuListItemAccount.style.visibility = 'visible';
+			menuListItemCart.style.visibility = 'visible';
+		}
 	} else {
 		navigationMenu.style.height = '0px';
+		if (window.screen.width <= 375){
+			menuListItemAccount.style.visibility = 'hidden';
+			menuListItemCart.style.visibility = 'hidden';
+		}
 	}
+	
     navigationMenuClose.onclick = function () {
 		navigationMenu.style.height = '0px';
 		navigationMenuOpen.checked = false;
+		navigationMenuOpen.onclick();
     }
 }
 
